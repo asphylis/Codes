61406544c770b3117d654fef1b1e47ef92facc08
asphylis <90554655+asphylis@users.noreply.github.com>
2021-09-19 22:57:49 +0530

    Add files via upload

---
+++ input.py
@@ -0,0 +1,12 @@
+'''
+python code to gain input from user and learn about mathematical operators
+'''
+print("Enter first integer value :")
+a = int(input())
+print("Enter second integer value :")
+b = int(input())
+
+print("Addition :", a+b)
+print("Subtraction :", a-b)
+print("Multiplication :", a*b)
+print("Division :", a/b)
