82267d4938b6033d43ea2bf9c68e2c7857da31b5
asphylis <90554655+asphylis@users.noreply.github.com>
2021-10-09 10:52:09 +0530

    Add files via upload

---
+++ basicdatastr.py
@@ -0,0 +1,25 @@
+''' 
+    A program to show basic data structure such as
+    Strings, Lists, Dictionaries, tuples, sets
+'''
+
+#string 
+str = 'This is the 1st string'
+print(str)
+
+#Lists - unordered, can contain both numbers and strings
+mylist = [1, 'two', 3, 4, 5]
+print(mylist)
+
+#Dictionaries - pair of key and value seperated by :
+dict ={'key1' : 'value1', 'key2': 'value2'}
+print(dict)
+
+#tuples - ordered and unchangeable
+tup = ('a', 'a', 'b', 'c')
+print(tup)
+
+#set - unordered and uniquesable
+myset = set()
+myset.add('a')
+print(myset)
