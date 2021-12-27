6df84311de95b73c7ad8d6bf3d9184c5d23ff5bf
asphylis <90554655+asphylis@users.noreply.github.com>
2021-12-27 21:08:09 +0530

    Update Guessing_game.py

--- Guessing_game.py
+++ Guessing_game.py
@@ -1,10 +1,15 @@
+#Declaring and initializing variables
 secret_number = '9'
-print("Guess the correct digit between 0-9\t'Hint: multiple of 3'\nYou only have 3 guesses!!")
 guess_count = 0
 guess_limit = 3
+#Printing Instructions
+print("Guess the correct digit between 0-9\t'Hint: multiple of 3'\nYou only have 3 guesses!!")
+#Beginning of "while" loop
 while guess_count < guess_limit  :
     chance = input("Guess :")
+    #Incrementing counter for every turn
     guess_count += 1
+    #Comparing Input and Secret number
     if chance == secret_number:
         print("Congratulations! You guessed the right number")
         break
