6f68368e7eee3c12aa69dc7b47c996b3a5f6240c
Ashwath Mahajan <90554655+asphylis@users.noreply.github.com>
2022-01-01 19:56:20 +0530

    Create stringoperations.py

---
+++ stringoperations.py
@@ -0,0 +1,12 @@
+#string functions
+str = '     "Hello World"          '
+#function to remove space from left side
+print(str.lstrip())
+#function to remove space from right side
+print(str.rstrip())
+#function to remove space from both sides
+print(str.strip())
+
+str_1 = 'This part was concatinated(Without any space in frozenset)'
+#for string concatination
+print("\n\n" + str + str_1)
