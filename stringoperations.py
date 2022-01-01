b9944632b18fa100fb5af0b79604435ccacd02b2
Ashwath Mahajan <90554655+asphylis@users.noreply.github.com>
2022-01-01 20:02:12 +0530

    Update stringoperations.py

--- stringoperations.py
+++ stringoperations.py
@@ -1,5 +1,6 @@
 #string functions
 str = '     "Hello World"          '
+print(str)
 #function to remove space from left side
 print(str.lstrip())
 #function to remove space from right side
@@ -10,3 +11,12 @@ print(str.strip())
 str_1 = 'This part was concatinated(Without any space in frozenset)'
 #for string concatination
 print("\n\n" + str + str_1)
+
+
+#Function to find the string length
+length = len(str_1)
+print(str_1 + "\nLength of this string : " + length)
+
+#Function to find position of a character in string
+print(str)
+print("\nPosition of W : " + str.find('W')
